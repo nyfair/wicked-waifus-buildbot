615ef90a0c7a274340d4736083e0a78fa4536682
nyfair <nyfair2012@gmail.com>
2025-05-18 15:37:45 +0800

    init

---
+++ start_all.bat
@@ -0,0 +1,6 @@
+pgsql\bin\pg_ctl start -D pgsql\database
+start /b wicked-waifus-config-server
+start /b wicked-waifus-game-server
+start /b wicked-waifus-gateway-server
+start /b wicked-waifus-hotpatch-server
+start /b wicked-waifus-login-server
